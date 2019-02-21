92ee6912bc74077a48b19b8915d1e5c5102127e3
imkunals726 <kunals726@gmail.com>
2019-02-21 21:04:08 +0530

    database connection code addition

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var express = require( 'express' );
+var mongoose = require( 'mongoose'   );
+
+mongoose.connect( 'mongodb://localhost:27017/SocMan' , { useNewUrlParser : true } , function( err , res ){
+	if( err ){
+		console.log( 'Error Connecting Database ..');
+	}else{
+		console.log( 'Success...' );
+	}
+});
+
+
 app 		= express( );
 
 
