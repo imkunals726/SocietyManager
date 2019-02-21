60d79aaf3c5f3b47a6fda5475bf9ea8bfeb7b765
imkunals726 <kunals726@gmail.com>
2019-02-21 18:18:40 +0530

    version 2

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require( 'express' );
 app 		= express( );
-app.listen(process.env.PORT, process.env.IP, function(){
+
+
+
+app.set('view engine' ,'ejs' );
+app.use(express.static('/public' ) );
+
+app.get('/' , function( req , res ){
+	res.render("index");
+});
+
+app.listen(3001, 'localhost', function(){
     console.log("Server has started!!!");
 });
