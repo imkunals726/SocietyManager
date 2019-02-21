d0ef8628117e553813819ef756d212e8b1c4680d
imkunals726 <kunals726@gmail.com>
2019-02-22 01:32:27 +0530

    Apartment Listing changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require( 'express' );
 var mongoose = require( 'mongoose'   );
 
-mongoose.connect( 'mongodb://localhost:27017/SocMan' , { useNewUrlParser : true } , function( err , res ){
+/*mongoose.connect( 'mongodb://localhost:27017/SocMan' , { useNewUrlParser : true } , function( err , res ){
 	if( err ){
 		console.log( 'Error Connecting Database ..');
 	}else{
@@ -10,17 +10,26 @@ mongoose.connect( 'mongodb://localhost:27017/SocMan' , { useNewUrlParser : true 
 });
 
 
+var SocietySchema = new mongoose.Schema({
+	Society_id 	: number,
+	Name		: string,
+	Addr		: string,
+	Rooms		: list,
+})*/
+
+var host = 'localhost';
+var port = 3001;
+var path = 'http://' + host + ':' + port
+
 app 		= express( );
-
-
-
+console.log( __dirname + '\\public' )
 app.set('view engine' ,'ejs' );
-app.use(express.static('/public' ) );
+app.use( '/public' , express.static( __dirname + '\\public' ) );
 
 app.get('/' , function( req , res ){
-	res.render("index");
+	res.render("index",{ Societies : [ { Name : 'Sairatna Apt' , Addr : 'Sagarli' } , { Name : 'Sairaj' , Addr : 'Sagarli' } ] } );
 });
 
-app.listen(3001, 'localhost', function(){
+app.listen(port, host, function(){
     console.log("Server has started!!!");
 });
